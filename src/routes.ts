57ca88705601d78eb11c0c4b7f01a0645697928a
Avery Sommer <asommer999@gmail.com>
2022-12-31 07:14:55 -0500

    init commit

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import Home from './views/Home.vue'
+import NotFound from './views/NotFound.vue'
+import Day from './views/Day.vue'
+
+export const routes = [
+  { path: '/', component: Home, meta: { title: 'Home' }},
+]
